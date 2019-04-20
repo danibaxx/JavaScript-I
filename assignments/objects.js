0804b3477b351b6e93db883d81b1511a58a83aae
Danielle B <danibaxx@hotmail.com>
2019-04-20 14:31:16 -0600

    updated objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -77,11 +77,13 @@ console.log(employeeFive.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+employeeTwo.speak = () => "Hello, my name is Kennan!";
+console.log(employeeTwo.speak());
 
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+employeeFive.multiplyNums = (a,b) => a * b;
+console.log(employeeFive.multiplyNums(3,4));
 
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
